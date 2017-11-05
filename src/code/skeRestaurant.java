25e321e53aef5ebdc7c2414fbb4369f7f541e7ef
KorawitRupanya <smart1micky268@gmail.com>
2017-11-05 11:35:24 +0700

    fixed

--- src/code/skeRestaurant.java
+++ src/code/skeRestaurant.java
@@ -1,4 +1,4 @@
-package code;
+package src.code;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -10,50 +10,56 @@ import java.util.Scanner;
  * @author Korawit Rupanya
  */
 
-public class skeRestaurant {
+public class SkeRestaurant {
 
 	static RestaurantManager theManager = new RestaurantManager();
 	static Scanner my = new Scanner(System.in);
-	static double tt = 0;
-	public final static String[] menuItems = { "Pizza", "Chickens", "Coke" };
-	private static final double[] unitPrice = { 250.0, 120.0, 45.0 };
-	static double[] order = { 0, 0, 0 };
+	static int quantity = 0;
+	static String[] menuItems = { "Pizza", "Chickens", "Coke" };
+	static double[] unitPrice = { 250.0, 120.0, 45.0 };
+	static double[] totalQuantity = new double[menuItems.length];
+	static double[] price = new double[menuItems.length];
+
+	public static void getPrice(int quan) {
+		quantity = my.nextInt();
+		totalQuantity[quan - 1] += quantity;
+		price[quan - 1] = totalQuantity[quan - 1] * unitPrice[quan - 1];
+	}
+
+	public static int totalPrice() {
+		int tt = 0;
+		for (int i = 0; i < menuItems.length; i++) {
+			tt += price[i];
+		}
+
+		return tt;
+	}
 
 	// this method print receipt with discount for membership
 	public static void receiptwithmember() {
-		tt = unitPrice[0] * order[0] + unitPrice[1] * order[1] + unitPrice[2] * order[2];
 		System.out.println("+------------Here is you receipt---------+");
 		System.out.println("+------ Menu -----+-- Qty --+-- Price ---+");
-		if (order[0] >= 0) {
-			System.out.printf("| %s\t\t   |   %.2f |  %.2f    |\n", menuItems[0], order[0], unitPrice[0] * order[0]);
-		}
-		if (order[1] >= 0) {
-			System.out.printf("| %s\t   |   %.2f |  %.2f    |\n", menuItems[1], order[1], unitPrice[1] * order[1]);
-		}
-		if (order[2] >= 0) {
-			System.out.printf("| %s\t\t   |   %.2f |  %.2f    |\n", menuItems[2], order[2], unitPrice[1] * order[1]);
+		for (int i = 0; i < menuItems.length; i++) {
+			if (totalQuantity[i] != 0) {
+				System.out.printf("| %-17s|%9f|%11f|\n", menuItems[i], totalQuantity[i], price[i]);
+			}
 		}
 		System.out.println("+------------------+---------+-----------+");
-		System.out.printf("| member discount%%10         |    %.2f  |\n", tt - (tt * 0.9));
-		System.out.printf("| Total\t\t             |    %.2f |\n", (tt) * 0.9);
+		System.out.printf("| member discount%%10         |    %.2f  |\n", totalPrice() - (totalPrice() * 0.9));
+		System.out.printf("| Total\t\t             |    %.2f |\n", (totalPrice()) * 0.9);
 		System.out.println("*------------------+---------+-----------*");
 	}
 
 	// this method print total for check total prices
 	public static void total() {
 		System.out.println("+------ Menu -----+-- Qty --+-- Price ---+");
-		tt = unitPrice[0] * order[0] + unitPrice[1] * order[1] + unitPrice[2] * order[2];
-		if (order[0] >= 0) {
-			System.out.printf("| %s\t\t   |   %.2f |  %.2f    |\n", menuItems[0], order[0], unitPrice[0] * order[0]);
-		}
-		if (order[1] >= 0) {
-			System.out.printf("| %s\t   |   %.2f |  %.2f    |\n", menuItems[1], order[1], unitPrice[1] * order[1]);
-		}
-		if (order[2] >= 0) {
-			System.out.printf("| %s\t\t   |   %.2f |  %.2f    |\n", menuItems[2], order[2], unitPrice[2] * order[2]);
+		for (int i = 0; i < menuItems.length; i++) {
+			if (totalQuantity[i] != 0) {
+				System.out.printf("| %-17s|%9d|%11d|\n", menuItems[i], totalQuantity[i], price[i]);
+			}
 		}
 		System.out.println("+------------------+---------+-----------+");
-		System.out.printf("| Total\t\t             |  %.2f    |\n", tt);
+		System.out.printf("| Total\t\t             |  %.2f    |\n", totalPrice());
 		System.out.println("*------------------+---------+-----------*");
 	}
 
@@ -64,10 +70,16 @@ public class skeRestaurant {
 		if (num1 == ran1) {
 			System.out.println("WOW!!! You are real SKE and and a real gambler too 555!!!");
 			System.out.println("+------------Here is you receipt---------+");
-			order[0] = 0;
-			order[1] = 0;
-			order[2] = 0;
-			total();
+			System.out.println("+------ Menu -----+-- Qty --+-- Price ---+");
+			for (int i = 0; i < menuItems.length; i++) {
+				if (totalQuantity[i] != 0) {
+					System.out.printf("| %-17s|%9d|%11d|\n", menuItems[i], totalQuantity[i], price[i]);
+				}
+			}
+			System.out.println("+------------------+---------+-----------+");
+			System.out.printf("| Total\t\t             |  %.2f    |\n", 0);
+			System.out.println("*------------------+---------+-----------*");
+			
 		} else {
 			System.out.println("Sorry...You miss it!");
 			System.out.println("+------------Here is you receipt---------+");
@@ -78,9 +90,10 @@ public class skeRestaurant {
 	// this method print menu
 	public static void intro() {
 		System.out.println("--------- Welcome to SKE Restaurant ---------");
-		System.out.printf("1.%s\t\t  %.2f Baht.%n", menuItems[0], unitPrice[0]);
-		System.out.printf("2.%s\t  %.2f Baht.%n", menuItems[1], unitPrice[1]);
-		System.out.printf("3.%s\t\t  %.2f Baht.%n", menuItems[2], unitPrice[2]);
+		for (int x = 0; x < menuItems.length; x++) {
+			System.out.printf("%d. %s\t\t%.2f Baht.\n", x + 1, menuItems[x], unitPrice[x]);
+		}
+		;
 		System.out.println("4.tatal");
 		System.out.println("5.Get Receipt");
 		System.out.println("6.The SKE promotion (Try this after you have already done your order.)");
@@ -96,18 +109,9 @@ public class skeRestaurant {
 				System.out.print("==========Thank you==========");
 				break;
 			} else if (choice >= 1 && choice < 4) {
-
 				System.out.print("Enter Quantity: ");
 				int quan = my.nextInt();
-				if (choice == 1) {
-					order[0] += quan;
-				}
-				if (choice == 2) {
-					order[1] += quan;
-				}
-				if (choice == 3) {
-					order[2] += quan;
-				}
+				getPrice(choice);
 			} else if (choice == 4) {
 				total();
 			} else if (choice == 5) {
